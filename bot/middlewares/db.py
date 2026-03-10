567eebc87e15838d8a21de29ef6b27b049709cd2
xojiakbarasomoff <khoji712@gmail.com>
2026-03-10 12:31:22 +0500

    feat: add database middleware

---
+++ bot/middlewares/db.py
@@ -0,0 +1,16 @@
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from db.database import AsyncSessionLocal
+
+
+class DbSessionMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        async with AsyncSessionLocal() as session:
+            data["db"] = session
+            return await handler(event, data)
